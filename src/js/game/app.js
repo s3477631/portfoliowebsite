a74334f7d464d5e73d6853af16bed824a63dc35e
Devin <devinwreeks@gmail.com>
2019-09-21 15:46:52 +1000

    significant folder restructure

---
+++ src/js/game/app.js
@@ -0,0 +1,49 @@
+import Buildings from "./buildings.js/index.js.js.js.js";
+
+
+let GAME_HEIGHT = 600;
+let GAME_WIDTH = 900;
+let canvas = document.getElementById("gamecanvas")
+
+
+let skyscrapers = ['../assets/bluebuilding.png', '../assets/greenbuilding.png', '../assets/purplebuilding.png', '../assets/redbuilding.png', '../assets/stripebuilding.png', '../assets/yellowbuilding.png']
+canvas.height = GAME_HEIGHT; 
+canvas.width = GAME_WIDTH;
+
+// canvas.setAttribute("style", "height: 100%; width: 100%;")
+let layer0 = canvas.getContext("2d")
+let ctx = canvas.getContext("2d");
+
+
+var backgroundImage = new Image();
+backgroundImage.onload = function () {  
+layer0.drawImage(backgroundImage, 0, 0, 900, 600);
+buildings.draw(ctx, skyscrapers);
+
+};
+backgroundImage.src = "../assets/backgroundgame.jpg";
+// function loaded() {
+//   // woo yey image loaded
+// }
+
+// if (img1.complete) {
+//   loaded();
+// }
+// else {
+//   ck.addEventListener('load', loaded);
+// }
+
+// img1.addEventListener('error', function() {
+//   // argh everything's broken
+// });
+
+
+
+ctx.clearRect(0, 0, GAME_HEIGHT, GAME_WIDTH);
+
+let buildings = new Buildings(GAME_WIDTH, GAME_HEIGHT);
+
+
+
+
+
---
+++ src/js/routes/routes.js
@@ -0,0 +1,24 @@
+function gamepage(){
+  return  window.location.href='./gamepage.html'
+}
+function aboutme(){
+    return  window.location.href='./html/aboutme.html'
+  }
+function workingprojects(){
+    return  window.location.href='./html/ruok.html'
+  }
+function startpage(){
+    return  window.location.href='./html/index.html'
+}
+
+function blogpost(){
+    return window.location.href='./html/blogpost.html'
+}
+
+function completedprojects(){
+  return window.location.href='./html/completeproj.html'
+}
+
+function opencamera(){
+  return window.location.href='./ruokcamera.html'
+}
